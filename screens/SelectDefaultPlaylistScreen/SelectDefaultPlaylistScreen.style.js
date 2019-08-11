0b43a7d28eea96f3a9c21cb4b335c8f39d1a8f77
Hrutvik Patel <hrutvikk@uoguelph.ca>
2019-08-11 15:52:39 -0400

    added basic styling for SelectDefaultPlaylistScreen

--- screens/SelectDefaultPlaylistScreen/SelectDefaultPlaylistScreen.style.js
+++ screens/SelectDefaultPlaylistScreen/SelectDefaultPlaylistScreen.style.js
@@ -10,11 +10,19 @@ export default StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'space-around',
         alignItems: 'center',
-        backgroundColor: theme.PRIMARY_COLOR
+        backgroundColor: theme.PRIMARY_COLOR,
+        borderWidth: 0
     },
-    searchBar: {
-        width: '90%',
-        backgroundColor: 'transparent'
+    searchBarContainer: {
+        width: '95%',
+        backgroundColor: 'transparent',
+        borderTopWidth: 0,
+        borderBottomWidth: 0,
+        marginTop: theme.SPACE_SMALL,
+        marginBottom: theme.SPACE_SMALL
+    },
+    searchBarInputContainer: {
+        borderRadius: theme.BORDER_ROUND,
     },
     listContainer: {
         flex: 6
@@ -22,9 +30,24 @@ export default StyleSheet.create({
     placeHolderTextColor: {
         color: '#b2bec3'
     },
-    text: {
+    headingText: {
         color: theme.COLOR_WHITE,
-        fontSize: 150,
-        fontFamily: theme.FONT_FAMILY_BOLD
+        fontSize: theme.FONT_SIZE_EXTRA_SMALL,
+        fontFamily: theme.FONT_FAMILY_BOLD,
+        marginTop: theme.SPACE_SMALL,
+        marginBottom: theme.SPACE_SMALL
+    },
+    item: {
+        width: '95%',
+        borderBottomWidth: 1,
+        borderBottomColor: '#b2bec3',
+        marginLeft: theme.SPACE_EXTRA_SMALL,
+        marginRight: theme.SPACE_EXTRA_SMALL,
+        paddingLeft: theme.SPACE_EXTRA_SMALL,
+        paddingBottom: theme.SPACE_EXTRA_SMALL,
+        paddingTop: theme.SPACE_EXTRA_SMALL,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignSelf: 'center'
     }
 });
